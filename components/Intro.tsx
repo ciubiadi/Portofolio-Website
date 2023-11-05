20c78b9547b284f8df681519acbb0531230bca98
Adrian Albert Ciubucciu <aciubucciu@deloittece.com>
2023-11-05 21:20:35 +0200

    create and finish buttons for CV, contact and social

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -1,8 +1,14 @@
 "use client";
-import Image from 'next/image'
-import React from 'react'
+
 import meImage from '@/public/me.jpeg'
-import { motion } from 'framer-motion';
+import Image from "next/image";
+import React from "react";
+import { motion } from "framer-motion";
+import Link from "next/link";
+import { BsArrowRight, BsLinkedin } from "react-icons/bs";
+import { HiDownload } from "react-icons/hi";
+import { FaGithubSquare } from "react-icons/fa";
+
 
 const Intro = () => {
   return (
@@ -26,7 +32,7 @@ const Intro = () => {
                     />
                 </motion.div>                
                 <motion.span 
-                    className='text-4xl absolute bottom-0 right-0'
+                    className='text-4xl absolute bottom-[-4px] right-[-4px]'
                     initial={{ opacity: 0, scale: 0}}
                     animate={{ opacity: 1, scale: 1}}
                     transition={{
@@ -54,6 +60,67 @@ const Intro = () => {
             building <span className="italic">sites & apps</span>. My focus is{" "}
             <span className="underline">React (Next.js)</span>.
         </motion.p>
+
+        <motion.div
+            className="flex flex-col sm:flex-row items-center justify-center 
+            gap-2 px-4 text-lg font-medium"
+            initial={{ opacity: 0, y: 100 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{
+                delay: 0.1,
+            }}
+        >
+            <Link
+                href="#contact"
+                className="group bg-gray-900 text-white px-7 py-3 flex 
+                items-center gap-2 rounded-full outline-none focus:scale-110 
+                hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+                onClick={() => {
+                    console.log('Go to contact')
+                }}
+            >
+                Contact me here{" "}
+                <BsArrowRight 
+                    className="opacity-70 group-hover:translate-x-1 transition" 
+                />
+            </Link>
+
+            <a
+                className="group bg-white px-7 py-3 flex items-center gap-2 
+                rounded-full outline-none focus:scale-110 hover:scale-110 
+                active:scale-105 transition cursor-pointer borderBlack 
+                dark:bg-white/10"
+                href="/CV.pdf"
+                download
+            >
+                Download CV{" "}
+                <HiDownload
+                className="opacity-60 group-hover:translate-y-1 transition" 
+                />
+            </a>
+
+            <a
+                className="bg-white p-4 text-gray-700 hover:text-gray-950 
+                flex items-center gap-2 rounded-full focus:scale-[1.15] 
+                hover:scale-[1.15] active:scale-105 transition cursor-pointer 
+                borderBlack dark:bg-white/10 dark:text-white/60"
+                href="https://www.linkedin.com/in/adrian-albert-ciubucciu/"
+                target="_blank"
+            >
+                <BsLinkedin />
+            </a>
+
+            <a
+                className="bg-white p-4 text-gray-700 flex items-center gap-2 
+                text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] 
+                hover:text-gray-950 active:scale-105 transition cursor-pointer 
+                borderBlack dark:bg-white/10 dark:text-white/60"
+                href="https://github.com/ciubiadi"
+                target="_blank"
+            >
+                <FaGithubSquare />
+            </a>
+        </motion.div>
     </section>
   )
 }
