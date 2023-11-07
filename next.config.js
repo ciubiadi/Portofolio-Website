d2c42bb856cd6afd1338e8928d70e297754ddc91
Adrian Albert Ciubucciu <aciubucciu@deloittece.com>
2023-11-07 08:22:18 +0200

    fix action attribute for form

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    // in order for the action attribute to work properrly, and formData be passed
+    experimental: {
+        serverActions: true
+    }
+}
 
 module.exports = nextConfig
