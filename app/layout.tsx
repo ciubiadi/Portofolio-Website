35dcf4cf4a42218d3f87b0e790448b19a25f924e
Adrian Albert Ciubucciu <aciubucciu@deloittece.com>
2023-11-09 19:24:44 +0200

    resolve conflict merge

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 import ActiveSectionContextProvider from '@/context/active-section-context'
 import { Toaster } from 'react-hot-toast'
+import Footer from '@/components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -28,7 +29,10 @@ export default function RootLayout({
         <ActiveSectionContextProvider>
           <Header />
           {children}
+          <Footer />
+
           <Toaster position="top-center" toastOptions={{ duration: 4000 }}/>
+
         </ActiveSectionContextProvider>
       </body>
     </html>
